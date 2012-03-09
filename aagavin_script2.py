cb6ba4d7183894b227da588043b80a0f6ea4b6a4
aagavin <aagavin@gmail.com>
2012-03-09 12:29:38 -0500

    aagavin commit

--- aagavin_script2.py
+++ aagavin_script2.py
@@ -25,6 +25,7 @@ yea kinda messyish right now
 '''
 #class FridayFlair=boss
 class FridayFlair:
+  '''yea let declair some variables '''
   def __init__(self,username, password, thesubreddityouwant): 
     self.thesub       = thesubreddityouwant
     self.topcomments  =list()
@@ -38,16 +39,16 @@ class FridayFlair:
   def getTopPost(self):
     topPost = self.r.get_subreddit(self.thesub).get_top(limit=1, url_data={'t': 'week'})
     for i in topPost:
-      '''if i.author_flair_text==None:
+      if i.author_flair_text==None:
         self.r.get_subreddit(self.thesub).set_flair(i.author, 'I have no flair', '1')
       else:
-        self.r.get_subreddit(self.thesub).set_flair(i.author, i.author_flair_text, '1')'''
-      self.post='Top post of the week(gold flair):\n\n[%s][%s](%s){[%s comments](%s)} by [%s[%s]](http://www.reddit/u/%s)\n\n\n\n' % (i.ups-i.downs, i.title, i.url,i.num_comments,i.permalink, i.author,i.author_flair_text, i.author)
-      self.post=self.post+'\n\n\n\nTop Comment of the week(silver flair):\n\n[%s][%s](%s) by [%s](http://www.reddit.com/u/%s)\n' % (i.comments_flat[0].ups-i.comments_flat[0].downs,i.comments_flat[0],i.comments_flat[0].permalink,i.comments_flat[0].author,i.comments_flat[0].author)
-      '''if i.comments_flat[0].author_flair_text==None:
+        self.r.get_subreddit(self.thesub).set_flair(i.author, i.author_flair_text, '1')
+      self.post='Top post of the week(gold flair):\n\n---\n\n[%s][%s](%s){[%s comments](%s)} by [%s[%s]](http://www.reddit/u/%s)\n\n\n\n' % (i.ups-i.downs, i.title, i.url,i.num_comments,i.permalink, i.author,i.author_flair_text, i.author)
+      self.post=self.post+'\n\n\n\nTop Comment of the week(silver flair):\n\n---\n\n[%s][%s](%s) by [%s](http://www.reddit.com/u/%s)\n' % (i.comments_flat[0].ups-i.comments_flat[0].downs,i.comments_flat[0],i.comments_flat[0].permalink,i.comments_flat[0].author,i.comments_flat[0].author)
+      if i.comments_flat[0].author_flair_text==None:
         self.r.get_subreddit(self.thesub).set_flair(i.comments_flat[0].author, 'I have no flair', '2')
       else:
-        self.r.get_subreddit(self.thesub).set_flair(i.comments_flat[0].author, i.author_flair_text, '2')'''
+        self.r.get_subreddit(self.thesub).set_flair(i.comments_flat[0].author, i.author_flair_text, '2')
 
 
   # The name should explain it self
@@ -63,15 +64,16 @@ class FridayFlair:
         except AttributeError:
           continue
     self.topcomments=sorted(self.topcomments, key=itemgetter(1), reverse=True)
-    self.post=self.post+'\n\n\n\nTop Comments of the week: \n\n'
+    self.post=self.post+'\n\n\n\nTop Comments of the week:\n\n---\n\n'
     for j in self.topcomments[0:10]:
       self.post=self.post+'\n[%s][%s](%s) by [%s](http://www.reddit.com/u/%s)\n' % (j[0].ups-j[0].downs,j[0],j[0].permalink,j[0].author,j[0].author)
 
 
   #This will post it to the subreddit
   def postToSub(self):
-    self.r.submit('sasmodroom', '[Announcement] Friday Flair Awards (test)', text=self.post)
-    
+    self.r.submit(self.thesub, '[Announcement] Friday Flair Awards', text=self.post)
+    self.r.logout()
+
 u=raw_input("Enter your username: ")
 p=getpass.getpass("Enter your password: ")
 s=raw_input("Enter your subreddit: ")
